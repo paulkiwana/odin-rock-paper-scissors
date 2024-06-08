621b3318718d98f544e0710cd2e9f61d11875bf1
paulkiwana <paulkiwana6@gmail.com>
2024-06-09 01:01:10 +0300

    Added the computer choice function

--- RPS.js
+++ RPS.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+const getComputerChoice = () => {
+    const choices = ["Rock","Paper","Scissors"];
+   const randomIndex = Math.floor(Math.random() * choices.length);
+console.log(choices[randomIndex]);
+
+}
+getComputerChoice()
