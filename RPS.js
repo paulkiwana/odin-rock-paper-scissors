114b1a985bb5e6ac89c9b6f8a00b7c677293be26
paulkiwana <paulkiwana6@gmail.com>
2024-06-09 18:12:29 +0300

    finally done with the rock paper scissors counsel game, stay tuned for a full version with web UI interactivity

--- RPS.js
+++ RPS.js
@@ -1,23 +1,51 @@
 const getComputerChoice = () => {
-    const choices = ["Rock","Paper","Scissors"];
-   const randomIndex = Math.floor(Math.random() * choices.length);
-console.log("computer chooses " + choices[randomIndex]);
+    const choices = ["rock","paper","scissors"];
+   const computerChoice = Math.floor(Math.random() * choices.length);
+return choices[computerChoice];
 
 }
-getComputerChoice()
+//getComputerChoice()
 
 const getHumanChoice = () => {
-    const humanChoice = prompt("enter choice","Rock");
-    console.log("I choose " + humanChoice);
+    const choice = prompt("enter choice","Rock");
+    const humanChoice = choice.toLowerCase();
+   return humanChoice;
 }
-getHumanChoice()
-let humanScore = 0;
-let computerScore = 0;
+//getHumanChoice()
+
+
 
 const playRound = (humanChoice, computerChoice) => {
-
+if(humanChoice ===  computerChoice){
+return "Its a Tie"
+} else if(
+    (humanChoice === "rock" && computerChoice === "scissors")||
+    (humanChoice === "scissors" && computerChoice === "paper")|| 
+    (humanChoice === "paper" && computerChoice === "rock")){
+return `You win ${humanChoice} beats ${computerChoice}`
+} else{
+    return `You lose ${computerChoice} beats ${humanChoice}`
 }
-const humanSelection = getHumanChoice();
+}
+
+//playRound(humanSelection, computerSelection);
+
+const playGame = () => {
+    let humanScore = 0;
+let computerScore = 0;
+    for(let i = 0; i<5; i++){
+        const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+const score = playRound(humanSelection, computerSelection);
+console.log(score);        
+    
+if(score.includes("win")){
+    humanScore++
+} else if(score.includes("lose")){
+    computerScore++
+}
+}
+console.log(`Final Score - You: ${humanScore}, Computer: ${computerScore}`)
+}
+playGame()
